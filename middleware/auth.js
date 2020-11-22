64688f088697d3fa4b282b19ea553f5dfdc3a20b
Derek Hsu <derek800109@gmail.com>
2020-11-22 16:38:44 +0800

    feat: create auth

---
+++ middleware/auth.js
@@ -0,0 +1,11 @@
+module.exports = {
+    authenticator: (req, res, next) => {
+        // req.isAuthenticated() 是 Passport.js 提供的函式，會根據 request 的登入狀態回傳 true 或 false
+        if (req.isAuthenticated()) {
+            return next()
+        }
+        req.flash('warning_msg', '請先登入才能使用！')
+        // console.log("auth.js+" + req.flash)
+        res.redirect('/users/login')
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,19 @@ const router = express.Router()
 
 const users = require('./modules/users')
 const todos = require('./modules/todos')
+const auth = require('./modules/auth')
 const home = require('./modules/home')
+
+const { authenticator } = require('../middleware/auth')  // 掛載 middleware
 
 // -------------------------------------------------------------------------------------------
 
+router.use('/auth', auth)
 router.use('/users', users)
-router.use('/todos', todos)
-router.use('/', home)
+
+router.use('/todos', authenticator, todos)
+
+router.use('/', authenticator, home)
 
 // -------------------------------------------------------------------------------------------
 
