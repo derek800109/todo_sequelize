595317411f0787ffcd6ce412519545cbb9b69aeb
Derek Hsu <derek800109@gmail.com>
2020-11-22 17:34:25 +0800

    feat: edit

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -12,10 +12,10 @@ router.get('/new', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-    const userId = req.user._id
+    const UserId = req.user.id
     const name = req.body.name
 
-    return Todo.create({ name, userId })
+    return Todo.create({ name, UserId })
         .then(() => res.redirect('/'))
         .catch(error => console.log(error))
 })
@@ -29,6 +29,31 @@ router.get('/:id', (req, res) => {
         .catch(error => console.log(error))
 })
 
+// ------------------------------------------------------------------------------------------- edit
+
+router.get('/:id/edit', (req, res) => {
+    // const UserId = req.user.id
+    const id = req.params.id
+
+    return Todo.findByPk(id)
+        .then(todo => res.render('edit', { todo: todo.toJSON() }))
+        .catch(error => console.log(error))
+})
+
+router.put('/:id', (req, res) => {
+    // const userId = req.user._id
+    const id = req.params.id
+    const { name, isDone } = req.body
+
+    return Todo.findByPk(id)
+        .then(todo => {
+            todo.name = name
+            todo.isDone = isDone === 'on'
+            return todo.save()
+        })
+        .then(() => res.redirect(`/todos/${id}`))
+        .catch(error => console.log(error))
+})
 
 // -------------------------------------------------------------------------------------------
 
